7b5152d2829521d8c0b4edc0c0b4dbae845c4104
Henry Snopek <hhsnopek@gmail.com>
2017-09-26 16:44:52 -0400

    validate Twilio request

--- twilio.go
+++ twilio.go
@@ -1,9 +1,14 @@
 package fsmtwilio
 
 import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
 	"fmt"
 	"net/http"
 	"os"
+	"sort"
 	"time"
 
 	"github.com/go-carrot/fsm"
@@ -43,9 +48,53 @@ func buildRouter(store fsm.Store, stateMachine fsm.StateMachine, startState stri
 	return router
 }
 
-// TODO Validate request integrity https://www.twilio.com/docs/api/security#validating-requests
+// Validation Spec: https://www.twilio.com/docs/api/security#validating-requests
+func Validate(r *http.Request) bool {
+	r.ParseForm()
+
+	// Identify Scheme
+	scheme := r.URL.Scheme
+	if scheme == "" {
+		if r.TLS != nil {
+			scheme = "https"
+		} else {
+			scheme = "http"
+		}
+	}
+
+	// Build URL
+	url := scheme + "://" + r.Host + r.RequestURI
+
+	// Sort Params
+	keys := make([]string, 0, len(r.Form))
+	for k, _ := range r.Form {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	// Build Hash
+	var buffer bytes.Buffer
+	buffer.WriteString(url)
+	for _, k := range keys {
+		buffer.WriteString(k + r.FormValue(k))
+	}
+
+	// Hash w/sha1 and base64 encode
+	hash := hmac.New(sha1.New, []byte(os.Getenv("TWILIO_AUTH_TOKEN")))
+	hash.Write(buffer.Bytes())
+	result := base64.StdEncoding.EncodeToString(hash.Sum(nil))
+	expected := r.Header.Get("X-Twilio-Signature")
+
+	return result == expected
+}
+
 func IncomingMessage(store fsm.Store, stateMachine fsm.StateMachine, startState string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if valid := Validate(r); !valid {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
 		message := new(MessageReceivedCallback)
 		decoder := schema.NewDecoder()
 
@@ -53,11 +102,13 @@ func IncomingMessage(store fsm.Store, stateMachine fsm.StateMachine, startState 
 		err := r.ParseForm()
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 
 		err = decoder.Decode(message, r.PostForm)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 
 		// Get Traverser
@@ -86,5 +137,6 @@ func IncomingMessage(store fsm.Store, stateMachine fsm.StateMachine, startState 
 		traverser.SetCurrentState(newState.Slug)
 
 		w.WriteHeader(http.StatusOK)
+		return
 	}
 }
